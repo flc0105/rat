6bfa673f5023f6b3ce55342a2116dd5051c7d044
fangliangchen <fangliangchen@protonmail.com>
2026-03-16 13:45:24 +0800

    remove extra code

--- client/commands/base.py
+++ client/commands/base.py
@@ -25,26 +25,3 @@ class CommandBase(ABC):
         发送中间结果
         """
         self._send_result(status, result, eof)
-
-# import importlib.util
-# import os
-# from abc import ABC
-#
-# from client.config.config import JOB_PATH
-# from core.utils.client_util.reflection_util import get_main_class
-#
-#
-# class CommandBase(ABC):
-#     """命令基类，定义公共接口"""
-#
-#     def __init__(self, socket):
-#         self.socket = socket
-#         self.command_id = None
-#
-#         self.jobs = {}
-#
-#     def _send_final_result(self, status, result, eof=1):
-#         self.socket.send_result(self.command_id, status, result, eof)
-#
-#     def _send_interim_result(self, status, result, eof=0):
-#         self.socket.send_result(self.command_id, status, result, eof)
